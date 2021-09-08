2bf35b985d67e717ba48042f09aeab3012534a06
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-08 09:59:47 -0500

    set up form page

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
 
 const Form = ({ initialTodo, handleSubmit, buttonLabel, history }) => {
-  // The Form Data State
-
+  // ~~~~~ The Form Data State ~~~~~
   // Initiallize the form with the initialTodo state
   const [formData, setFormData] = useState(initialTodo);
 
-  return <h1>Blog Form</h1>;
+  // ~~~~~ Functions ~~~~~
+  // Standard React Form HandleChange Function
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  // Function to run when form is submitted
+  const handleSubmission = (event) => {
+    event.preventDefault(); // prevent form refresh
+    handleSubmit(formData); // pass formData to handleSubmit prop function
+    history.push("/"); // push user back to main page
+  };
+
+  return (
+    <form onSubmit={handleSubmission}>
+      {/* input field for subject */}
+      <input
+        type="text"
+        onChange={handleChange}
+        value={formData.subject}
+        name="subject"
+      />
+      {/* input field for details */}
+      <input
+        type="text"
+        onChange={handleChange}
+        value={formData.details}
+        name="details"
+      />
+      <input type="submit" value={buttonLabel} />
+    </form>
+  );
 };
 
 export default Form;
